e1ec3f7432a95f9b17b7406bc30551ab7faa4e0e
aashish21437 <ashishchauhan4636@gmail.com>
2026-04-04 02:46:05 +0530

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from "next";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 // We are removing the Geist and Geist_Mono imports here 
 // to stop the internal HTTP/2 network error.
@@ -16,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className="antialiased">
         {children}
       </body>
