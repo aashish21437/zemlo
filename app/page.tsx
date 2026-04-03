a9a04fd058b9e58f361029e6f8cf0ba907de4ea9
aashish21437 <ashishchauhan4636@gmail.com>
2026-04-04 04:18:30 +0530

    spelling mistake fized

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
                 <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-400 opacity-75"></span>
                 <span className="relative inline-flex rounded-full h-2.5 w-2.5 bg-emerald-500 shadow-inner"></span>
               </span>
-              <span>Available for new project</span>
+              <span>Available for new projects</span>
             </div>
           </div>
 
